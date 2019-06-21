6b5a9b751dd73627c2d86548856e84532f9cf230
Cauê Forlogic <caue@forlogic.net>
2019-06-21 18:02:53 -0300

    final commit

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -9,6 +9,19 @@ export default class Portfolio extends Component {
         return (
             <section id="portfolio">
                 <h2>PORTFÓLIO</h2>
+
+                <h3>Aqui Alguns de meus projeto:</h3>
+
+                <ul>
+                    <a href="https://github.com/caueaquino/contacts-book">1° - Contact Book</a>
+
+                    <a href="https://github.com/caueaquino/Inker-API-DataBase">2° - Inker</a>
+
+                    <a href="https://github.com/caueaquino/contacts-book">3° - Chapryt</a>
+
+                    <a href="https://github.com/caueaquino/caue-desafio-4-dev"> 4° - Web Avaliações</a>
+                </ul>
+
             </section>
         )
     }
