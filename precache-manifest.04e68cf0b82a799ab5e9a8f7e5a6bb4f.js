14c110a1583bc0d3b0d3c2eb100ea7fe100323bd
Cauê Forlogic <caue@forlogic.net>
2019-06-21 18:04:53 -0300

    Updates

---
+++ precache-manifest.04e68cf0b82a799ab5e9a8f7e5a6bb4f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102e9790a684c2e53738cef14b1e4ab5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d21ce86fe18bac997463",
+    "url": "/portfolio/static/css/main.4f10712a.chunk.css"
+  },
+  {
+    "revision": "f3193f1b8a6b46d32847",
+    "url": "/portfolio/static/js/2.3a026474.chunk.js"
+  },
+  {
+    "revision": "d21ce86fe18bac997463",
+    "url": "/portfolio/static/js/main.40100dc7.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ec26dfdc0ddc00975784d8063c85270b",
+    "url": "/portfolio/static/media/CaueAquinoNogueira_Curriculum.ec26dfdc.pdf"
+  },
+  {
+    "revision": "1ef527c1ad5819b0c6ebc6d7f2954e8a",
+    "url": "/portfolio/static/media/background-main.1ef527c1.jpg"
+  },
+  {
+    "revision": "f3d4d68fc041c5781b9c1d07bae6dfd2",
+    "url": "/portfolio/static/media/blog1.f3d4d68f.png"
+  },
+  {
+    "revision": "5c202bd52b1c6483480bc4d21f9abee8",
+    "url": "/portfolio/static/media/blog2.5c202bd5.png"
+  },
+  {
+    "revision": "f565e9a1a1f4e75a5886a3253f95c709",
+    "url": "/portfolio/static/media/blog3.f565e9a1.png"
+  },
+  {
+    "revision": "c972d3d7de268cd4113c678c14835452",
+    "url": "/portfolio/static/media/me.c972d3d7.JPG"
+  }
+]);
